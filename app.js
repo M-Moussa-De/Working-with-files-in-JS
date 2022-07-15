e3bbaa24585f2c59ecc826ad728dd64085196b6d
Mahmoud Moussa <m.moussa@ratio-software.de>
2022-07-15 16:57:43 +0200

    simple design

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const resultPlaceholder = document.querySelector("#result");
+const textBtn = document.querySelector("#text");
+const jsonBtn = document.querySelector("#json");
 
-//  Read file
-async function readFile() {
-  const res = await fetch("./file.json", {
+async function readFile(url, kind) {
+  const res = await fetch(url, {
     method: "GET",
     headers: {
-      "Content-Type": "text/plain",
+      "Content-Type": `${kind === "text" ? "text/plain" : "Application/json"}`,
     },
   });
 
-  const data = await res.text();
+  const data = `${kind === "text" ? res.text() : res.json()}`;
 
   console.log(data);
 }
 
-readFile();
+textBtn.addEventListener("click", () => {
+  readFile("file.txt", "text");
+});
+
+jsonBtn.addEventListener("click", () => {
+  readFile("file.json", "json");
+});
